3db9629312d2c117141a4fbe8851e62853c63dbd
bbasak-brainiumin <110466677+bbasak-brainiumin@users.noreply.github.com>
2024-05-21 16:08:59 +0530

    Update ajax-file-uploader.php

--- Theme_helper/ajax-file-uploader.php
+++ Theme_helper/ajax-file-uploader.php
@@ -1,1 +1,18 @@
+<?php 
+class WPAjaxHandler {
+  public function __construct($action) {
+    add_action( 'wp_ajax_nopriv_' . $action, array( $this, 'handle_request' ) );
+    add_action( 'wp_ajax_' . $action, array( $this, 'handle_request' ) );
+  }
+
+  public function handle_request() {
+    if ( isset( $_POST['action_type'] ) && method_exists( $this, $_POST['action_type'] ) ) {
+      call_user_func( array( $this, $_POST['action_type'] ), $_POST );
+    } else {
+      wp_send_json_error( array( 'message' => 'Invalid action type' ) );
+      wp_die();
+    }
+  }
+  
+}
 
