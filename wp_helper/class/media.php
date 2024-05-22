312cc1c5786df3cdb480a0c66ed9e513011e19d4
bbasak-brainiumin <110466677+bbasak-brainiumin@users.noreply.github.com>
2024-05-22 21:36:19 +0530

    Update media.php

--- wp_helper/class/media.php
+++ wp_helper/class/media.php
@@ -2,6 +2,7 @@
 class mediaHelper {
 
     public function __construct(){
+        add_action( 'wp_head', array( $this, 'ajaxurl_head' ), 99 );
         add_shortcode( 'bn_upload_helper', array( $this, 'file_uploader_render' ));
     }
 
@@ -57,4 +58,45 @@ class mediaHelper {
         
         }
     }
+    public function ajaxurl_head() {
+        echo '<script type="text/javascript">var ajaxurl = "' . admin_url('admin-ajax.php') . '";</script>';
+    }
+    public function media_footer_script() {
+        echo '<script>
+        jQuery(document).ready(function($) {
+            var myForm = $("#myform");
+             
+            $(myForm).submit(function(e) {
+                //Prevent normal form submission
+                e.preventDefault();
+                 
+                //Get the form data and store in a variable
+                var myformData = new FormData(myForm[0]);
+                 
+                //Add our own action to the data
+                //action is where we will be hooking our php function
+                myformData.append('action', 'pn_wp_frontend_ajax_upload');
+                 
+                //Prepare and send the call
+                $.ajax({
+                    type: "POST",
+                    data: myformData,
+                    dataType: "json",
+                    url: ajaxurl,
+                    cache: false,
+                    processData: false,
+                    contentType: false,
+                    enctype: 'multipart/form-data',
+                    success: function(data, textStatus, jqXHR) {
+                        //Success codes goes here
+                    },
+                    error: function(jqXHR, textStatus, errorThrown){
+                        //if fails     
+                        console.log(jqXHR);
+                    }
+                });
+            });
+        }); </script>';
+    }
+
 }
